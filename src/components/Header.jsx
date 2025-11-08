be5f4485c7cf95caf4947bc8ddf90be10f43cf27
Luzivam34 <luzivam.santos28@outlook.com>
2025-11-08 20:29:09 -0300

    criado o ambiente virtual luz_venv

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -12,6 +12,7 @@ export default function Header(){
                 <nav style={{ marginLeft: 'auto', display: 'flex', gap: '0.6rem' }}>
                     <Link to="/" className="button" style={{ background: 'transparent', color: 'var(--text)', padding: '0.4rem 0.6rem', borderRadius: 6, border: '1px solid rgba(0,0,0,0.06)' }}>Início</Link>
                     <Link to="/products" className="button">Produtos</Link>
+                    <Link to="/jogador" className="button">Jogador</Link>
                     <Link to="/about" className="button" style={{ background: 'transparent', color: 'var(--text)', border: '1px solid rgba(0,0,0,0.06)' }}>Sobre</Link>
                 </nav>
             </div>
